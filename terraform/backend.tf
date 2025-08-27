0556c98003775b244379203e63b2334fdfb5a51d
Abuharis Salih <abuharis.salih@gmail.com>
2025-08-27 22:46:35 +0300

    Bucket name corrected

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "demo-terraform-state-bucket"   # create this in Terraform bootstrap or manually
+    bucket         = "demo-tfstate-bucket-xyz"   # create this in Terraform bootstrap or manually
     key            = "cloud-ai-integration/demo.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "demo-terraform-lock"
